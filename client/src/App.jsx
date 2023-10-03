42c1bdafcb57ac04dce707fa8db72dc16adb42ce
Ashwini Kumar Pandey <pandeyash27@gmail.com>
2023-10-04 02:55:32 +0530

    another commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 function App() {
-  axios.defaults.baseURL = 'https://chat-app-7h0f.onrender.com/';
+  axios.defaults.baseURL = 'https://chat-app-7h0f.onrender.com';
   axios.defaults.withCredentials = true; //used so that we can set cookies from API
   return (
     <UserContextProvider>
